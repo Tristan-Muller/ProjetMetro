1d22aecd37239b5f672d7615a827da0c661ab39b
Muller-Tristan <muller_tristan@yahoo.fr>
2022-10-28 17:56:10 +0200

    17h55 28/10/22

--- liste.c
+++ liste.c
@@ -13,10 +13,10 @@ void detruire_liste_et_truc(Un_elem *liste);
 Un_elem *inserer_liste_trie(Un_elem *liste, Un_truc *truc){
 	Un_elem *tmp = (Un_elem*)malloc(sizeof(Un_elem));
 	tmp->truc=truc;
-	tmp->suivant=NULL;
+	tmp->suiv=NULL;
 
 	//Si problème d'allocation 
-	if(temp==NULL){
+	if(tmp==NULL){
 		printf("erreur d'allocation de tmp");
 	}
 
@@ -26,28 +26,28 @@ Un_elem *inserer_liste_trie(Un_elem *liste, Un_truc *truc){
 	}
 
 	Un_elem *precedent = liste;
-	Un_elem *element = liste->suivant;
+	Un_elem *element = liste->suiv;
 
 	//Cas où user_val du truc à ajouter est inférieur à la 1ère valeur user_val de la liste
 	if((liste->truc->user_val)>(tmp->truc->user_val)){
-		tmp->suivant = liste;
+		tmp->suiv = liste;
 		return tmp;
 	}
 
 	//Cas général
 	while(((element->truc->user_val)<(tmp->truc->user_val))&&(element!=NULL)){
-		precedent->suivant;
-		element->suivant;
+		precedent = precedent->suiv;
+		element = element->suiv;
 	}
 
 	//Cas où la liste ne contient qu'un seul terme
 	if(element==NULL){
-		precedent->suivant = tmp;
+		precedent->suiv = tmp;
 		return precedent; 
 	}
 
-	precedent->suivant = tmp
-	tmp->suivant = element;
+	precedent->suiv = tmp;
+	tmp->suiv = element;
 	return liste;
 }
 
