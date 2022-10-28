ec2ff6848fa48ea9ad01c7d054f578b36120aee9
Muller-Tristan <muller_tristan@yahoo.fr>
2022-10-28 15:50:18 +0200

    15h50 28/10/22

--- truc.c
+++ truc.c
@@ -5,10 +5,14 @@
 
 
 Un_truc *creer_truc(Une_coord coord, Ttype type, Tdata data, double uv){
-	
+	Un_truc new=(Un_truc*)malloc(sizeof(Un_truc));
+	new->coord = coord;
+	new->data = data;
+	new->user_val = uv;
+	return new;
 }
 
 
 void detruire_truc(Une_truc *truc){
 
-}
+}g
