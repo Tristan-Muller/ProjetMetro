bc95d9f5a7d09790b4c77be9309c7a6da6a4c4b1
Mairl1 <merlin.fouche@gmail.com>
2022-10-28 16:33:28 +0200

    Avance sur ex2

--- abr.c
+++ abr.c
@@ -3,7 +3,14 @@
 #include <truc.h>
 #include <abr_type.h>
 
-Un_nabr *creer_nabr(Un_truc *truc) {}
+Un_nabr *creer_nabr(Un_truc *truc) {
+    Un_nabr * nabr = (Un_nabr * ) malloc(sizeof(Un_nabr));
+    nabr->g = NULL;
+    nabr->d = NULL;
+    nabr-> truc = truc; 
+
+    return nabr;
+}
 
 
 
@@ -15,7 +22,18 @@ Un_nabr *construire_abr(Un_elem *liste_sta);{}
 
 
 
-void detruire_abr(Un_nabr *abr);{}
+void detruire_abr(Un_nabr *abr);{
+    if (! abr) retrun; 
+    
+    if (!abr->d && !abr->g) 
+        free(abr); 
+    
+    else {
+        if (abr->d) detruire_abr(abr->d);
+        if (abr->g) detruire_abr(abr->g);
+    }
+    
+}
 
 
 
