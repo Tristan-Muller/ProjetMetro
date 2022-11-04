01f1723728d62f67d1ae158dd032f66adf9fb00d
Mairl1 <merlin.fouche@gmail.com>
2022-11-04 11:05:02 +0100

    Avancée sur ligne.c

--- ligne.c
+++ ligne.c
@@ -6,34 +6,60 @@
 #include "liste.h"
 #include "truc.c"
 
+//lligne : liste de lignes
+
 Une_ligne *lire_lignes(char *nom_fichier){
+    /*parcours le fichier .csv fournis en argument et construit la liste de Une_ligne correspondante*/
 
-    FILE* fic = NULL;
- 
-    char code;
-    int v_moy = 0; 
-    float interval = 0;
-    char color[15];
- 
+    FILE* fic = NULL; 
     fic = fopen(nom_fichier, "r");
-
     if (!fic) return NULL; 
 
-    fscanf(fic, "%c;%d;%f;%s", &code, &v_moy, &interval, color);
+    char code[10];
+    float v_moy = 0; 
+    float interval = 0;
+    char color[15];
 
-    printf("\nCode : %c\nVitesse : %d\nIntervalle : %f\nCouleur : %s", code, v_moy, interval, color);
+    Une_ligne * lligne = NULL;  
 
-    
-    fscanf(fic, "%c;%d;%f;%s", &code, &v_moy, &interval, color);
 
-    printf("\nCode : %c\nVitesse : %d\nIntervalle : %f\nCouleur : %s", code, v_moy, interval, color);
-     //Ne marche pas pour l'instant mais je teste 
-
+    while (fscanf(fic, "%s ; %f ; %f ; %s", code, &v_moy, &interval, color) != EOF){
+        lligne = ajout_ligne(lligne, code, v_moy, interval, color);         
+    }
 
     fclose(fic);
-    
  
-    return NULL;
+    return lligne;
+}
+
+
+
+Une_ligne * ajout_ligne(Une_ligne *lligne, char *code, float v_moy, float interval, char *color){
+    /*fonction d'ajout d'une nouvelle ligne à une liste de ligne*/
+
+    Une_ligne * new = (Une_ligne *) malloc(sizeof(Une_ligne *));
+    if(!new){
+        printf("ERREUR\n");
+        return NULL;
+    } 
+
+    strcpy(new->code, code);
+    new->vitesse = v_moy;
+    new->intervalle = intervalle; 
+    strcpy(new->color, color);
+
+    if (!lligne){
+        return new; 
+    }
+
+    Une_ligne * temp = lligne;
+
+    while(temp->suiv){
+        
+    }
+
+
+    return lligne;     
 }
 
 
